2b3a6bd71ea6d797e2d72adde2dd20857ceb4789
Davaa <m.moo_gii_18@yahoo.com>
2021-12-08 10:18:19 +0800

    Event Enter btn

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ console.log("Дэлгэцнээс өгөгдлөө авах хэсэг:");
     //
 document.addEventListener("keypress", function(event){
 if (event.keyCode === 13 || event.which === 13) {ctrlAddItem();}
-})
+});
 })(uiController,financeController);
