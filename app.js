da842f75c3fa890ff7a62dbbb6c6a33757a5673d
Davaa <m.moo_gii_18@yahoo.com>
2021-12-12 22:28:40 +0800

    Finance module ruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ getDOMstrings: function(){
 
 // Санхүүтэй ажиллах контроллёр
 var financeController = (function(){
+    // priivate data
     var Income = function(id, description, value){
         this.id =id;
         this.description= description;
@@ -38,7 +39,7 @@ var financeController = (function(){
       };
 
       var data = {
-          allItems: {
+          items: {
               inc: [],
               exp: []
           },
@@ -46,15 +47,40 @@ var financeController = (function(){
               inc: 0,
               exp:0
           }
-      }
+      };
+
+      return {
+          addItem: function(type,desc,val){
+              var item,id;
+
+              if(data.items[type].length === 0) id=1;
+              else {
+                  id= data.items[type][data.items[type].length-1].id+1;
+              }
+
+              if(type === 'inc'){
+                  item= new Income(id, desc,val);
+              } else {
+                  item = new Expense(id,desc,val);
+              }
+              data.items[type].push(item);
+          },
+          seeData: function(){
+              return data;
+          }
+        };
+      
 })();
+//14.11  lesson 67g 
 
 // Програмын холбогч контроллёр
 var appController = (function(uiController,financeController){
     var ctrlAddItem = function() {
 // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
-console.log(uiController.getInput());
+var input = (uiController.getInput());
+console.log(input);
 // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллёрт дамжуулж тэнд хадгална.
+financeController.addItem(input.type,input.description, input.value);
 // 3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
 // 4. Төсвийг тооцоолно. 
 // 5. Эцсийн үлдэгдэль тооцоог дэлгэцэнд гаргана.
@@ -63,10 +89,7 @@ console.log(uiController.getInput());
 var setupEventListeners = function() {
     var DOM = uiController.getDOMstrings();
     document.querySelector(DOM.addBtn).addEventListener("click", function(){
-        ctrlAddItem();
-
-    });
-    //
+        ctrlAddItem();});
 document.addEventListener("keypress", function(event){
 if (event.keyCode === 13 || event.which === 13) {ctrlAddItem();}
 });
